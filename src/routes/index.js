7a41cae27d6cc60b0fadf24fc36995b03d822296
JoaquinDeLuca <joaqui222@gmail.com>
2023-01-20 19:27:54 -0300

    se agrega las rutas para borrar y editar las task

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,22 @@
 import { Router } from "express";
-import Task from '../models/Task'
+import Task from "../models/Task";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
 
-  const tasks = await Task.find().lean()
-
-  res.render("home",{tasks: tasks});
+  res.render("home", { tasks: tasks });
 });
 
 router.post("/tasks/add", async (req, res) => {
-
-  try{
+  try {
     const task = Task(req.body);
-  
-    await task.save();
-  
-    res.redirect('/')
 
-  }catch(error){
+    await task.save();
+
+    res.redirect("/");
+  } catch (error) {
     console.log(error);
   }
 });
@@ -28,8 +25,33 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit", (req, res) => {
-  res.render("edit");
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Para actulizar task seria method put
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id, req.body);
+
+  res.redirect("/");
+});
+
+// Para borrar una task seria method delete
+router.get("/delete/:id", async (req, res) => {
+
+  const { id } = req.params;
+
+  await Task.findByIdAndDelete(id);
+
+  res.redirect('/')
 });
 
 export default router;
