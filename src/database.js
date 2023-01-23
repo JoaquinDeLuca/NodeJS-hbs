689a28a8f1ca6487e05af1b2dff62b290d6a5b65
JoaquinDeLuca <joaqui222@gmail.com>
2023-01-23 20:19:23 -0300

    se agrego dotenv

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import {config} from 'dotenv';
+config();
 
 mongoose.set("strictQuery", false);
 
 (async () => {
     try{
-        const db = await mongoose.connect('mongodb+srv://joaquin:uUrWaMcSQTBdfKQt@clusterhbs.bhkypvy.mongodb.net/?retryWrites=true&w=majority')
+        const db = await mongoose.connect(process.env.MONGO_URI)
         console.log("DB connect to", db.connection.name);
 
     }catch(error){
